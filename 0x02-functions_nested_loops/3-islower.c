8fd4e40765bea7973f3f3a423b15a32768cb2310
Khalid Mohammed <khalarasta104@gmail.com>
2023-02-21 21:36:20 +0300

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _islowerl -  a function that checks for lowercase character.
+ *
+ * Return: Always 0.
+ */
+
+int _islowerl(int c)
+{
+	if (c > 96 && c < 123)
+	{
+		_putchar('0');
+	}
+	else
+	{
+		_putchar('1');
+	}
+}
