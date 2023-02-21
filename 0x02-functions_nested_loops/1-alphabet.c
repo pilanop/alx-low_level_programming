c8f3bcfdcc0a38896e32e95c5c8f068b3fbc00c5
Khalid Mohammed <khalarasta104@gmail.com>
2023-02-21 20:23:16 +0300

    Functions, nested loops

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,18 +1,6 @@
 #include "main.h"
 
 /**
- * main - check the code
- *
- * Return: Always 0.
- */
-
-int main(void)
-{
-	print_alphabet();
-	return (0);
-}
-
-/**
  * print_alphabet -  check the code
  *
  * Return: Always 0.
