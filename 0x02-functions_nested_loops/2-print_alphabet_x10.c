0b27a65ba9e1cc966837061832b0346741501ad1
Khalid Mohammed <khalarasta104@gmail.com>
2023-02-21 20:43:01 +0300

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 -  check the code
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet_x10(void)
+{
+	char i;
+	int loop;
+
+	for (loop = 0; loop <= 10; loop++)
+	{
+		for (i = 'a'; i <= 'z'; i++)
+		{
+			_putchar(i);
+		}
+	}
+	_putchar('\n');
+}
