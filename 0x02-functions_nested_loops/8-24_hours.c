0a2e3c5f5f42837efa587c3538763101bee3e798
Khalid Mohammed <khalarasta104@gmail.com>
2023-02-22 02:06:57 +0300

    24_hours

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minute of the day of Jack Bauer
+ *
+ * Return: returns starting from 00:00 to 23:59.
+ */
+void jack_bauer(void)
+{
+	int h1, h2, m1, m2;
+
+	for (h1 = 0; h1 <= 2; h1++)
+	{
+		for (h2 = 0; h2 <= 9; h2++)
+		{
+			if (h1 == 2 && h2 == 4)
+				break;
+
+			for (m1 = 0; m1 <= 5; m1++)
+			{
+				for (m2 = 0; m2 <= 9; m2++)
+				{
+					_putchar(h1 + '0');
+					_putchar(h2 + '0');
+					_putchar(':');
+					_putchar(m1 + '0');
+					_putchar(m2 + '0');
+					_putchar('\n');
+				}
+			}
+		}
+	}
+}
