5f6c37d39fc93ff3f0a44f6fe60cfc70e803f64d
Khalid Mohammed <khalarasta104@gmail.com>
2023-02-23 02:25:40 +0300

    100-times_table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -16,7 +16,6 @@ void print_times_table(int n)
 		if (n > 15 || n < 0)
 			break;
 		_putchar('0');
-
 		for (column = 1; column <= n; column++)
 		{
 			result = row * column;
@@ -40,11 +39,13 @@ void print_times_table(int n)
 				_putchar(result % 10 + '0');
 			}
 			else
+			{
 				_putchar(',');
-			_putchar(' ');
-			_putchar(result / 100 + '0');
-			_putchar((result / 10) % 10 + '0');
-			_putchar(result % 10 + '0');
+				_putchar(' ');
+				_putchar(result / 100 + '0');
+				_putchar((result / 10) % 10 + '0');
+				_putchar(result % 10 + '0');
+			}
 		}
 		_putchar('\n');
 	}
