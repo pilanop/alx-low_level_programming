640343b435b99f5c867202bfd02df4c0868cc2b6
Khalid Mohammed <khalarasta104@gmail.com>
2023-02-23 03:22:12 +0300

    102-fibonacci

---
+++ 101-natural.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints the sum of all the multiples of 3 or 5 below 1024 (excluded), followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i, j = 0;
+	for (i = 0; i < 1024; ++i)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			j += i;
+		}
+	}
+	printf("%d", j);
+}
---
+++ 102-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	long int n1 = 1, n2 = 2, fib;
+
+	printf("%ld, %ld", n1, n2);
+
+	for (i = 2; i < 50; i++)
+	{
+		fib = n1 + n2;
+		printf(", %ld", fib);
+		n1 = n2;
+		n2 = fib;
+	}
+
+	printf("\n");
+
+	return (0);
+}
