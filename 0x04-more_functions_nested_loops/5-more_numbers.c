67dcaf53b8c65de35d9a30bfc6693ec134f1f499
Khalid Mohammed <khalarasta104@gmail.com>
2023-02-24 20:49:54 +0300

    more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -2,22 +2,22 @@
 
 /**
  * more_numbers - a function that prints an integer.
- *
+ * 
  * Return: void
  */
 
 void more_numbers(void)
 {
 
-	int i, j;
+int i, j;
 	for (i = 1; i <= 10; i++)
 	{
 		for (j = 0; j <= 14; j++)
 		{
 			if (j >= 10)
-				_putchar('1');
+			_putchar('1');
 			_putchar(j % 10 + '0');
 		}
-		_putchar('\n');
-	}
+			_putchar('\n');
+		}
 }
