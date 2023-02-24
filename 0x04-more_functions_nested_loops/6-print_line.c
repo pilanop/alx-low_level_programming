b27e18becb5d444e22fe73612df5ff7f875431fe
Khalid Mohammed <khalarasta104@gmail.com>
2023-02-24 03:31:23 +0300

    print_line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -10,11 +10,13 @@ void print_line(int n)
 {
 	int i;
 
-	for (i = 0; i <= n; ++i)
+	if (n > 0)
 	{
-		if (i <= 0)
-			_putchar('\n');
-		_putchar('_');
+		for (i = 0; i < n; i++)
+			_putchar('_');
 	}
-	_putchar('\n');
+	else
+	{
+		_putchar('\n');
+	}
 }
