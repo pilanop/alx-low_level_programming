064a21b1e1a617251ccfdc92b5f54964cc784152
Khalid Mohammed <khalarasta104@gmail.com>
2023-02-28 04:16:42 +0300

    4-print_rev

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,12 +10,18 @@
 
 void print_rev(char *s)
 {
-	int i = _strlen(s) - 1;
+	int i = 0;
 
-	while (i >= 0)
+	while (s[i] != '\0')
 	{
-		_putchar(s[i]);
-		i--;
+		i++;
+	}
+	int j = i - 1;
+
+	while (j >= 0)
+	{
+		_putchar(s[j]);
+		j--;
 	}
 	_putchar('\n');
 }
