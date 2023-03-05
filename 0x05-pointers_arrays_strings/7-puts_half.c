3faf5a9bce88cfd2dd0b29c606fee02832bbab7c
Khalid Mohammed <khalarasta104@gmail.com>
2023-03-05 03:12:19 +0300

    7-puts-half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -19,7 +19,7 @@ void puts_half(char *str)
 	}
 
 	if (length % 2 != 0)
-		for (i = ((length - 1) / 2); i < length; i++)
+		for (i = ((length - 1) / 2) + 1; i < length; i++)
 		{
 			_putchar(str[i]);
 		}
