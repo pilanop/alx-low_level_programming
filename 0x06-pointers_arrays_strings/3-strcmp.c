64728bbaf3e1269706ef0f987bb09624376924be
Khalid Mohammed <khalarasta104@gmail.com>
2023-03-06 00:59:24 +0300

    3-strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,19 +9,8 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	while (((*s1 != '\0') && (*s2 != '\0')) && (*s1 == *s2))
-	{
-		s1++;
-		s2++;
-	}
-
-	if (*s1 == *s2)
-	{
-		return (0);
-	}
-
-	else
-	{
+	if (*s1 < *s2 || *s1 > *s2)
 		return (*s1 - *s2);
-	}
+	else
+		return (0);
 }
