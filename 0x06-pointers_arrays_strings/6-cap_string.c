b0a08e8a197f2be2ffc71b72b6ec6772f0842db8
Khalid Mohammed <khalarasta104@gmail.com>
2023-03-06 04:46:47 +0300

    6-cap_string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,41 +9,25 @@
  */
 char *cap_string(char *s)
 {
-	int i = 0;
-	int r;
-	int len;
-	char up;
-	int length = 0;
-	int j = 0;
-	char arr[] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
+	int i = 0, j;
+	char a[] = " \t\n,;.!?\"(){}";
 
-	while (s[j] != '\0')
+	while (*(s + i))
 	{
-		length++;
-		j++;
-	}
-
-	while (i <= length)
-	{
-
-		if (s[i] == arr[0] || s[i] == arr[1] || s[i] == arr[2] || s[i] == arr[3] || s[i] == arr[4] || s[i] == arr[5] || s[i] == arr[6] || s[i] == arr[7] || s[i] == arr[8] || s[i] == arr[9] || s[i] == arr[10] || s[i] == arr[11] || s[i] == arr[12])
+		if (*(s + i) >= 'a' && *(s + i) <= 'z')
 		{
-			if (s[i + 1] > 97)
-			{
-				len = s[i + 1] - 97;
-
-				up = 65 + len;
-
-				s[i + 1] = up;
-
-				i++;
-			}
+			if (i == 0)
+				*(s + i) -= 'a' - 'A';
 			else
-				i++;
+			{
+				for (j = 0; j <= 12; j++)
+				{
+					if (a[j] == *(s + i - 1))
+						*(s + i) -= 'a' - 'A';
+				}
+			}
 		}
-		else
-			i++;
+		i++;
 	}
-
 	return (s);
 }
