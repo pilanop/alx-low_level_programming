ece410786448df93faae0b7504bcda0709697184
Khalid Mohammed <khalarasta104@gmail.com>
2023-03-07 03:03:25 +0300

    2-strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _memset - a function that locates a character in a string.
+ *
+ * @c: The character to locate in the string
+ * @s: The string to search
+ *
+ * Return: a pointer to the first occurrence of the character @c
+ * 	   in the string @s, or NULL if the character is not found
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return s;
+		}
+		s++;
+	}
+	if (*s == c)
+	{
+		return s;
+	}
+	return NULL;
+}
