aaf1eddf2e8d786f9e024451b13ec8945b5770b8
Khalid Mohammed <khalarasta104@gmail.com>
2023-03-21 02:54:01 +0300

    dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,17 @@
 #ifndef _DOG_H_
 #define _DOG_H_
 
+/**
+ * struct dog - dog class
+ *
+ * @name: Name of the dog
+ *
+ * @age: Age of the dog
+ *
+ * @owner: Owner of the dog
+ * 
+ */
+
 struct dog
 {
     char *name;
