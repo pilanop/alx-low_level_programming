345f5183a7a01c9f401136827bec05ba339f2a97
Khalid Mohammed <khalarasta104@gmail.com>
2023-03-22 03:33:11 +0300

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -24,8 +24,6 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 void free_dog(dog_t *d);
 
-typedef struct dog dog_t;
-
 dog_t *new_dog(char *name, float age, char *owner);
 
 #endif
