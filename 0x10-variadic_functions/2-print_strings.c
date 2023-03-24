4c3b7c56804c38a966bfd677b1a4eb149e4f1b50
Khalid Mohammed <khalarasta104@gmail.com>
2023-03-24 05:45:34 +0300

    print_strings

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -2,11 +2,8 @@
 
 /**
  * print_strings - Prints strings passed to the function separed by separators
- *
  * @separator: A string that is used to separate strings
- *
  * @n: number of unnamed parameters
- *
  * Return: Always 0
  */
 
