569393bac999885e32c248109f86947412470203
Khalid Mohammed <khalarasta104@gmail.com>
2023-04-03 03:22:43 +0300

    print_list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -19,10 +19,15 @@ size_t print_list(const list_t *h)
 		i++;
 		ptr = ptr->next;
 	}
+
 	if (h->str == NULL)
+	{
 		printf("[0] (nil)\n");
+	}
 	else
+	{
 		printf("[%i] %s\n", h->len, h->str);
+	}
 
 	return i;
 }
