d21cdda64ba0737ddc1e661d9a3069d0317dce7b
Khalid Mohammed <khalarasta104@gmail.com>
2023-04-11 02:55:32 +0300

    error fix

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * binary_to_uint -converts a binary number to an unsigned int.
+ * binary_to_uint - converts a binary number to an unsigned int.
  * @b: pointing to a string of 0 and 1 chars
  *
- * Return:converted number, or 0
+ * Return: converted number, or 0
  */
 unsigned int binary_to_uint(const char *b)
 {
