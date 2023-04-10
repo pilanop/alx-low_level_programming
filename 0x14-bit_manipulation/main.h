32a90d1a427fce493fc05ee315906320eff9c673
Khalid Mohammed <khalarasta104@gmail.com>
2023-04-11 02:55:44 +0300

    main.h

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,8 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
-unsigned long int _power(unsigned int base, unsigned int pow);
+
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
+unsigned long int _power(unsigned int base, unsigned int pow);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
